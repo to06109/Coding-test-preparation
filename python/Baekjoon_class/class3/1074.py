e3a33bc9de937dc3a18039fd1b0c998c37e14900
to06109 <69625013+to06109@users.noreply.github.com>
2022-05-22 16:59:07 +0900

    Update 1074, before refactoring

--- python/Baekjoon_class/class3/1074.py
+++ python/Baekjoon_class/class3/1074.py
@@ -1,60 +1,48 @@
 # BOJ 1074 Z
-# 틀린 코드 -> 사분면으로 나눠서 분할정복으로 풀어야하는듯
-# 반례: 5, 22, 22 -> 정답: 828, 내 답: 876
+# 72ms
+# 30840KB
+# 리팩토링 전 코드
 N, r, c = map(int, input().split())
-
-length = 2 ** N - 1
-half = 2 ** (N-1) - 1
 
 count = 0
 
-def counting(start_r, start_c):
-    cnt = 0
+# 분할정복(재귀)
+def counting(start_r, start_c, n): # r, c 시작점, 각 내부 사각형
+    global count
     
-    # 몇번째 4개짜린지 찾기
-    dif_r = r - start_r
-    dif_c = c - start_c
-    # 현재 위치 전 상자까지의 개수
-    num = (dif_r // 2) * ((half + 1) // 2) + (dif_c // 2)
+    length = 2 ** n - 1
+    half = 2 ** (n-1) - 1
     
-    cnt += num * 4
-
-    # 현재 4개짜리에서 몇번째인지 찾기
-    # 현재 위치 시작위치 찾기 -> num+1번째 상자의 시작위치
-    s_r = start_r + (dif_r // 2) * 2
-    s_c = start_c + (dif_c // 2) * 2
-
-    diff_r = r - s_r
-    diff_c = c - s_c
-    if diff_r == 0 and diff_c == 0:
-        cnt += 1
-    elif diff_r == 0 and diff_c == 1:
-        cnt += 2
-    elif diff_r == 1 and diff_c == 0:
-        cnt += 3
+    if n == 1:
+        # 거기서부터 좌표 찾으면서 누적
+        if r == start_r and c == start_c:
+            count += 1
+        elif r == start_r and c == start_c + 1:  
+            count += 2
+        elif r == start_r + 1 and c == start_c:
+            count += 3
+        else:
+            count += 4
+        return
+        
+    half_num = (half+1) * (half+1)
+    # 1사분면
+    if start_r <= r <= start_r + half and start_c <= c <= start_c + half:
+        # 개수세기
+        counting(start_r, start_c, n - 1)
+    # 2사분면
+    elif start_r <= r <= start_r + half and start_c + half < c <= start_c + length:
+        count += half_num
+        counting(start_r, start_c + half + 1, n - 1)
+    # 3사분면
+    elif start_r + half < r <= start_r + length and start_c <= c <= start_c + half:
+        count += 2 * half_num
+        counting(start_r + half + 1, start_c, n - 1)
+    # 4사분면
     else:
-        cnt += 4
+        count += 3 * half_num
+        counting(start_r + half + 1, start_c + half + 1, n - 1)
     
-    return cnt
-
-half_num = (half+1) * (half+1)
-# 1사분면
-if 0 <= r <= half and 0 <= c <= half:
-    # 개수세기
-    count += counting(0, 0)
-# 2사분면
-elif 0 <= r <= half and half < c <= length:
-    count += half_num
-    count += counting(0, half + 1)
-# 3사분면
-elif half < r <= length and 0 <= c <= half:
-    count += 2 * half_num
-    count += counting(half + 1, 0)
-# 4사분면
-else:
-    count += 3 * half_num
-    count += counting(half + 1, half + 1)
+counting(0, 0, N)
 
 print(count - 1) # 카운팅을 0부터 시작하므로
-
-# 예제는 다 맞았는데 틀렸습니다 뜸
