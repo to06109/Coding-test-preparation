40f1d617706518e7f6846e8e95baa49bc25a59ae
to06109 <69625013+to06109@users.noreply.github.com>
2022-05-23 22:27:01 +0900

    0523_prepare

--- python/Baekjoon_class/class3/1074.py
+++ python/Baekjoon_class/class3/1074.py
@@ -1,20 +1,19 @@
 # BOJ 1074 Z
 # 72ms
 # 30840KB
-# 리팩토링 전 코드
 N, r, c = map(int, input().split())
 
 count = 0
 
-# 분할정복(재귀)
-def counting(start_r, start_c, n): # r, c 시작점, 각 내부 사각형
+# 재귀
+def counting(start_r, start_c, n): # r, c 시작점, N
     global count
     
     length = 2 ** n - 1
-    half = 2 ** (n-1) - 1
+    half = 2 ** (n - 1) - 1
     
     if n == 1:
-        # 거기서부터 좌표 찾으면서 누적
+        # Z에서 카운팅
         if r == start_r and c == start_c:
             count += 1
         elif r == start_r and c == start_c + 1:  
@@ -28,7 +27,7 @@ def counting(start_r, start_c, n): # r, c 시작점, 각 내부 사각형
     half_num = (half+1) * (half+1)
     # 1사분면
     if start_r <= r <= start_r + half and start_c <= c <= start_c + half:
-        # 개수세기
+        # 4분면으로 다시 쪼개고 개수세기
         counting(start_r, start_c, n - 1)
     # 2사분면
     elif start_r <= r <= start_r + half and start_c + half < c <= start_c + length:
@@ -45,4 +44,4 @@ def counting(start_r, start_c, n): # r, c 시작점, 각 내부 사각형
     
 counting(0, 0, N)
 
-print(count - 1) # 카운팅을 0부터 시작하므로
+print(count - 1) # 카운팅을 0부터 시작하므로 1 빼주기
