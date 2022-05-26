a791f8ceaebc944829eff2ab0856ad864ae34156
Yesong <to06109@naver.com>
2022-05-26 10:35:54 +0900

    0526_prepare sol 가장 큰 수

---
+++ python/Programmers_kit/sort/가장 큰 수.py
@@ -0,0 +1,6 @@
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x*3, reverse=True)
+    return str(int(''.join(numbers)))
+
+# https://jokerldg.github.io/algorithm/2021/05/06/most-big-number.html
