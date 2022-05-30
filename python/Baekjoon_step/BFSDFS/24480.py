7c227672bdaa3a10c6dbac31013a74b74d927842
Yesong <to06109@naver.com>
2022-05-30 11:30:45 +0900

    0530_prepare sol 24480

--- python/Baekjoon_step/BFSDFS/24480.py
+++ python/Baekjoon_step/BFSDFS/24480.py
@@ -1,36 +1,39 @@
-# BOJ 24479 알고리즘 수업 - 깊이 우선 탐색 1
-# 700ms
-# 156956KB
+# BOJ 24480 알고리즘 수업 - 깊이 우선 탐색 2
+# 인접 정점 내림차순
+# 712ms
+# 156952KB
+
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 N, M, R = map(int, input().split())
-result = [0] * (N + 1)
-# 그래프 만들기
 graph = [[] for _ in range(N + 1)]
 for i in range(M):
     u, v = map(int, input().split())
-    # 양방향 간선
     graph[u].append(v)
     graph[v].append(u)
 
-# 인접정점 오름차순 방문
-for j in range(N + 1):
-    graph[j].sort()
+for j in graph:
+    j.sort(reverse=True) # 내림차순 정렬
 
 visited = [False] * (N + 1)
+result = [0] * (N + 1)
 count = 1
-def DFS(r):
+
+def DFS2(r):
+
     global count
+
     # 방문처리
     visited[r] = True
     result[r] = count
-    # 현재 노드와 연결되어있는 노드 순차탐색
+
+    # 현재 노드와 인접한 노드 탐색
     for i in graph[r]:
         if visited[i] == False:
             count += 1
-            DFS(i)
+            DFS2(i)
 
-DFS(R) 
-
-for i in range(1, N + 1):
-    print(result[i])
+DFS2(R)
+for m in range(1, N + 1):
+    print(result[m])
