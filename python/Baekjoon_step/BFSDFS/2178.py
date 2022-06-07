d19432dd7f9d0faa2ee0633f22abebf493e326c0
to06109 <69625013+to06109@users.noreply.github.com>
2022-06-07 23:02:58 +0900

    0607_prepare sol 2178

--- python/Baekjoon_step/BFSDFS/2178.py
+++ python/Baekjoon_step/BFSDFS/2178.py
@@ -1,6 +1,10 @@
 # BOJ 2178 미로 탐색
-# DFS -> 최단거리 못찾음 
+# BFS로 현재 노드에서 상하좌우에 있는 노드에 거리 1씩 더하기
+# 104ms
+# 32476KB
+
 import sys
+from collections import deque
 input = sys.stdin.readline
 N, M = map(int, input().split())
 graph = [input().rstrip() for _ in range(N)]
@@ -11,25 +15,25 @@ dy = [-1, 1, 0, 0]
 
 result = []
 visited = [[False] * M for _ in range(N)]
-def DFS(x, y, dis): # 현재 위치(열, 행), 거리
-    
+
+def BFS(x, y, dis): # 현재 위치(열, 행), 거리
+    queue = deque()
+    queue.append([x, y])
     # 방문처리
-    visited[y][x] = dis
+    visited[y][x] = dis + 1
     
-    # 리턴조건
-    if x == M - 1 and y == N - 1:
-        result.append(dis)
-        return
-    
-    # 상하좌우에 있는지 확인
-    for i in range(4):
-        next_x = x + dx[i]
-        next_y = y + dy[i]
-        if 0 <= next_x < M and 0 <= next_y < N:
-            if not visited[next_y][next_x] and graph[next_y][next_x] == "1":
-                visited[next_y][next_x] = visited[y][x] + 1
-                DFS(next_x, next_y, visited[next_y][next_x])
-                
+    while queue:
+        node = queue.popleft()
+        # 상하좌우에 있는지 확인
+        for i in range(4):
+            next_x = node[0] + dx[i]
+            next_y = node[1] + dy[i]
+            if 0 <= next_x < M and 0 <= next_y < N:
+                if not visited[next_y][next_x] and graph[next_y][next_x] == "1":
+                    # 방문처리2
+                    queue.append([next_x, next_y])
+                    visited[next_y][next_x] = visited[node[1]][node[0]] + 1
+                          
 distance = 0
-DFS(0, 0, distance)
-print(result)
+BFS(0, 0, distance)
+print(visited[N-1][M-1])
