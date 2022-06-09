111abd7bb07baef9f518c6c20e522954a18f7ab4
to06109 <69625013+to06109@users.noreply.github.com>
2022-06-09 21:04:36 +0900

    0609_prepare sol 7576

--- python/Baekjoon_step/BFSDFS/7576.py
+++ python/Baekjoon_step/BFSDFS/7576.py
@@ -1,5 +1,7 @@
 # BOJ 7576 토마토
-# 풀이 중. 처음에 1이 여러 개 있을 때 동시에 BFS 시작하는 것을 구현하지 못함
+# 2312ms
+# 106160KB
+
 import sys
 from collections import deque
 input = sys.stdin.readline
@@ -7,44 +9,41 @@ M, N = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(N)]
 
 # 상하좌우
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
-
-visited = [[False] * M for _ in range(N)]
-
-count = 0
-def BFS(x, y):
-    
-    global count
-    
-    queue = deque()
-    queue.append([x, y])
-    # 방문처리
-    visited[y][x] = True
-    
+dx = [0, 0, -1, 1] # 열
+dy = [-1, 1, 0, 0] # 행
+# 정답이 들어갈 변수
+count = 0 
+# 좌표를 넣을 것이므로 deque에 []를 넣어줌
+queue = deque([])
+# 처음에 받은 토마토의 위치 좌표를 queue에 append.
+for i in range(N):
+    for j in range(M):
+        if graph[i][j] == 1:
+            queue.append([j, i])
+            
+def BFS():
     while queue:
+        # 토마토의 위치좌표 꺼내기
         node = queue.popleft()
         for i in range(4):
             next_x = node[0] + dx[i]
             next_y = node[1] + dy[i]
-            if 0 <= next_x < M and 0 <= next_y < N:
-                if graph[next_y][next_x] == 0 and visited[next_y][next_x] == False:
-                    queue.append([next_x, next_y])
-                    graph[next_y][next_x] = graph[node[1]][node[0]] + 1
-                    visited[next_y][next_x] = True
-        count += 1
-    print(node)
+            # 해당 좌표가 graph의 크기를 넘어가면 안되고 그 좌표애 토마토가 익지 않은 채로 있어야 함(0)
+            if 0 <= next_x < M and 0 <= next_y < N and graph[next_y][next_x] == 0:
+                queue.append([next_x, next_y])
+                graph[next_y][next_x] = graph[node[1]][node[0]] + 1
 
-for i in range(N):
-    for j in range(M):
-        if graph[i][j] == 1 and not visited[i][j]:
-            BFS(j, i)
+BFS()
 
-# BFS 다 끝났는데 0이 있으면 -1 출력
+# BFS 다 끝났는데 0이 존재하면 -1 출력
 for m in graph:
     for n in m:
         if n == 0:
             print(-1)
-            break
+            exit(0)
+    # 0이 없다면 graph의 최대값이 정답이 됨
+    count = max(count, max(m))
 
-print(graph)
+print(count - 1)
+
+# 참고링크: https://jae04099.tistory.com/entry/%EB%B0%B1%EC%A4%80-7576-%ED%86%A0%EB%A7%88%ED%86%A0-%ED%8C%8C%EC%9D%B4%EC%8D%AC-%ED%95%B4%EC%84%A4%ED%8F%AC%ED%95%A8
