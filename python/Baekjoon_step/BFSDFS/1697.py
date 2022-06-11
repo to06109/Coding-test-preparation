c5a4ba536ce1c1f22fc5982db83dd1cf15b2d21a
to06109 <69625013+to06109@users.noreply.github.com>
2022-06-11 23:10:53 +0900

    0611_prepare sol 1697

--- python/Baekjoon_step/BFSDFS/1697.py
+++ python/Baekjoon_step/BFSDFS/1697.py
@@ -1,19 +1,24 @@
 # BOJ 1697 숨바꼭질
-# 메모리초과
+# 152ms
+# 34692KB
+
 from collections import deque
 N, K = map(int, input().split())
 # 수빈이가 갈 수 있는 곳 다 찾기??
-queue = deque([])
+queue = deque()
 def BFS():
-    queue.append([N, 0]) # 수빈이 위치, 간 거리
+    queue.append(N) # 수빈이 위치
     while queue:
-        cur, cnt = queue.popleft()
-        print(cur)
-        if cur == K:
-            print(cnt)
-            break
-        queue.append([2 * cur, cnt + 1])
-        queue.append([cur + 1, cnt + 1])
-        queue.append([cur - 1, cnt + 1])
+        cur = queue.popleft()
+        # 동생위치 도착하면 함수 종료
+        if cur == K: 
+            return
         
+        for i in (cur - 1, cur + 1, cur * 2):
+            if 0 <= i < 100001 and dp[i] == 0:
+                dp[i] = dp[cur] + 1
+                queue.append(i)
+
+dp = [0] * 100001
 BFS()
+print(dp[K])
