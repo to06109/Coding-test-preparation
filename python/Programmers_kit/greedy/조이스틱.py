c2b00092a44b607a2990ce9a7e86e4748c6a944a
to06109 <69625013+to06109@users.noreply.github.com>
2022-06-22 03:21:36 +0900

    0622_prepare fail to solve 조이스틱2

--- python/Programmers_kit/greedy/조이스틱.py
+++ python/Programmers_kit/greedy/조이스틱.py
@@ -1,36 +1,25 @@
 # Programmers 조이스틱
+# 굳이 인덱스를 앞으로 뒤로 옮기지 않고 문자열 순서대로 탐색하는 것을 기본으로하고
+# 처음 A가 나왔을 때만 cnt를 알맞게 변경해주는 방식 시도함
 # 실패...
 name = input()
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-index = 0 # name의 index
-cnt = 0 # 조이스틱 조작 횟수
-visit = [False] * len(name)
-back = False
+cnt = 0 # 조이스틱 조작 횟수\
+first = True # A를 처음 만나는가?
 
-while (False in visit):
-    if index == -1:
-        index = len(name) - 1
-    if not visit[index]: # 방문한 적이 없다면?
-        letter = name[index]
-        idx = alphabet.index(letter)
-        if letter == "A" and not back: # 거꾸로가기
-            cnt -= 3 # 문자열 마지막으로 가는 데 필요한 조작횟수
-            visit[index] = True
-            index = len(name) - 1 # 문자열 마지막으로 가기
-            back = True
-        else:
-            if idx > 12: # 알파벳 뒤에부터 찾는 게 최소횟수
-                cnt += 26 - idx
-            else:
-                cnt += idx
-            visit[index] = True
-        
-    if back:
-        index -= 1
+for i in range(len(name)):
+    letter = name[i]
+    idx = alphabet.index(letter)
+    if letter == "A" and first: # 거꾸로가기
+            cnt += i - 2 # 문자열 마지막으로 가는 데 필요한 조작횟수
+            first = False
     else:
-        index += 1 
-    cnt += 1 # 좌우로 움직이는거?
-
-answer = cnt
-
-print(cnt-1)
+        if idx > 12: # 알파벳 뒤에부터 찾는 게 최소횟수
+            cnt += 26 - idx
+        else:
+            cnt += idx
+    cnt += 1
+print(cnt - 1)\
+    
+# https://bellog.tistory.com/152
+# A가 여러 개일 경우, 여러 A가 연속적으로 붙어있을 경우를 생각하지 않음 
