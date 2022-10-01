46974a408cef4a590558d064761597589e1aed88
to06109 <69625013+to06109@users.noreply.github.com>
2022-10-01 18:19:15 +0900

    1001_prepare sol 구명보트 py

--- python/Programmers_level/level2/구명보트.py
+++ python/Programmers_level/level2/구명보트.py
@@ -1,18 +1,24 @@
-# 실패코드 다시 풀어야함
 from collections import deque
 def solution(people, limit):
     people.sort()
     queue = deque(people)
     result = 0
+    
     while queue:
-        total = 0
-        while queue and total <= limit:
-            value = queue.popleft()
-            total += value
+        mini = queue[0]
+        maxx = queue[-1]
+        
+        # 큐에 한 사람 밖에 없을 경우
+        if len(queue) == 1:
+            result += 1
+            break
+        # 두 명 다 태울 경우
+        if mini + maxx <= limit:
+            queue.popleft()
+        # 두 명 못태우는 경우 -> 최대값만 태움
+        queue.pop()
         result += 1
-        if total > limit:
-            queue.appendleft(value)
-            
+    
     return result
 
 '''
@@ -21,6 +27,8 @@ people을 정렬한 다음 가장 몸무게가 가장 큰 사람과
 작으면 둘 다 태우고, 크면 가장 큰 사람만 태운다.
 
 이 때 주의할 점은 indexing으로 비교하는 것이 아니라, 
-pop 혹은 del, remove등 리스트 자체에서 빼버리면 
-정답은 맞을지라도 효율성 부분에서 틀려버린다.
+리스트에서 pop, del, remove등을 사용해 빼면 효율성 부분에서 틀린다.
+
+하지만 deque의 pop을 사용하면 시간복잡도가 O(1)으로 
+요소를 뺄 수 있으므로 효율성 테스트에 통과한다. 
 '''
