ec1c644ed1f11b86c0fd992bad9106d1d8c669b4
to06109 <69625013+to06109@users.noreply.github.com>
2022-10-23 23:53:52 +0900

    1023_prepare solving 괄호 회전하기 js

---
+++ Javascript/Programmers_level/level2/괄호 회전하기.js
@@ -0,0 +1,49 @@
+// 13, 14 테케 실패
+
+function solution(s) {
+  const hash = {
+    ']': '[',
+    '}': '{',
+    ')': '(',
+  }
+
+  // 문자열 회전 함수
+  const rotate = (text, noOfChars = 0) => {
+    const first = text.slice(noOfChars)
+    const second = text.slice(0, noOfChars)
+    return first + second
+  }
+
+  // 괄호문자열 판별 함수
+  const isRight = (text) => {
+    const count = {
+      '[': 0,
+      '{': 0,
+      '(': 0,
+    }
+    const arr = [...text]
+    const values = Object.values(hash)
+    for (let letter of arr) {
+      if (values.includes(letter)) {
+        count[letter] += 1
+      } else {
+        count[hash[letter]] -= 1
+        if (count[hash[letter]] < 0) return false
+      }
+    }
+
+    const count_values = Object.values(count)
+    count_values.forEach((item) => {
+      if (item !== 0) return false
+    })
+    return true
+  }
+
+  let result = 0
+  for (let i = 0; i < s.length; i++) {
+    const s_rotated = rotate(s, i)
+    if (isRight(s_rotated)) result += 1
+  }
+
+  return result
+}
